8f607a2e4ac211a0332fa15afd1b461f5404ce88
kinsomicrote <kinsomicrote.inc@gmail.com>
2017-10-01 13:41:22 +0100

    user registration added

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,7 +1,5 @@
+const AuthenticationController = require('./controllers/AuthenticationController')
+
 module.exports = (app) => {
-  app.post('/register', (req, res) => {
-    res.send({
-      message: `Hello ${req.body.email} Successfully registered!`
-    })
-  })
+  app.post('/register', AuthenticationController.register)
 }
